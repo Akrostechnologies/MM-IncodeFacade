96f0190910fb9005839d701e1521c78f850f1f60
Roberto De la O <pocasangre870@gmail.com>
2024-10-30 11:17:33 -0600

    :lipstick: REV-5280 Change top bar icon

--- Sources/MMIncodeFacade/Modules/Signature/views/DocumentPreview/DocumentPreview.swift
+++ Sources/MMIncodeFacade/Modules/Signature/views/DocumentPreview/DocumentPreview.swift
@@ -54,7 +54,7 @@ struct DocumentPreview: View {
         .navigationBarHidden(false)
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
-            ToolbarItem(placement: .topBarLeading) { toolbarButtons }
+            ToolbarItem(placement: .topBarTrailing) { toolbarButtons }
         }
     }
     
@@ -93,7 +93,7 @@ struct DocumentPreview: View {
     @ViewBuilder
     var toolbarButtons: some View {
         Button { onClose?() } label: {
-            Image("left-icon", bundle: .current)
+            Image("x-mark-icon", bundle: .current)
                 .renderingMode(.template)
                 .resizable(capInsets: .init(top: 0, leading: 0, bottom: 0, trailing: 0), resizingMode: .stretch)
                 .aspectRatio(contentMode: .fill)
